98a2e644c467c722dfedb522821a8bd8491aa842
Manish Kumar <matangs@yahoo.com>
2015-10-17 23:12:04 -0400

    Receipt activity work started. Static strings added in the string array. Date picker is showing up. Need to figure out how to get the the value back.

--- XpenseRcpt/app/src/main/java/com/xpensercpt/mkumar/xpensercpt/ReceiptActivity.java
+++ XpenseRcpt/app/src/main/java/com/xpensercpt/mkumar/xpensercpt/ReceiptActivity.java
@@ -1,13 +1,52 @@
 package com.xpensercpt.mkumar.xpensercpt;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class ReceiptActivity extends AppCompatActivity {
+
+    public static class MyDatePickerFragment extends AppCompatDialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +63,93 @@ public class ReceiptActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        setStatusBarColor();
+        setSaveButton();
     }
+
+    public void showDatePickerDialog(View v) {
+
+        AppCompatDialogFragment newFragment = new MyDatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+        //newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    private void setSaveButton(){
+        Button saveButton = (Button) findViewById(R.id.saveRcptButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                if (name.isEmpty()) {
+
+                    new AlertDialog.Builder(AddTripActivity.this)
+                            .setTitle("Nothing to add")
+                            .setMessage("Did you forget to enter a name for the trip?")
+                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // continue with delete
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                    return;
+
+                }
+                */
+
+                /*
+                TripDataSource dm = new TripDataSource(AddTripActivity.this);
+                dm.open();
+                dm.insertTrip(name, date);
+                dm.close();
+                */
+                ReceiptActivity.this.finish();
+
+            }
+        });
+    }
+
+
+    @TargetApi(21)
+    void setStatusBarColor(){
+        if (Build.VERSION.SDK_INT < 21)
+            return;
+
+        Window window = this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.darkest_orange));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
