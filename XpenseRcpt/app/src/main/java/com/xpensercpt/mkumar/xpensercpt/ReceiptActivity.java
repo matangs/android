9377c988cb6dc6900914456856ddc27fff76a0a2
Manish Kumar <matangs@yahoo.com>
2016-01-09 22:47:05 -0500

    removing settings menu

--- XpenseRcpt/app/src/main/java/com/xpensercpt/mkumar/xpensercpt/ReceiptActivity.java
+++ XpenseRcpt/app/src/main/java/com/xpensercpt/mkumar/xpensercpt/ReceiptActivity.java
@@ -730,7 +730,7 @@ try {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
